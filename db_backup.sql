393f4540ca598c55ad9902dcef8a010d7e85f0ba
preet-maiya <mail.maiyapreetham@gmail.com>
2017-11-28 18:00:47 +0530

    Updated sql

--- db_backup.sql
+++ db_backup.sql
@@ -56,7 +56,8 @@ CREATE TABLE `booking` (
   KEY `violationno` (`violationno`),
   KEY `police` (`police`),
   CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`violationno`) REFERENCES `violations` (`no`),
-  CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`police`) REFERENCES `police_login` (`username`)
+  CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`police`) REFERENCES `police_login` (`username`),
+  CONSTRAINT `booking_ibfk_3` FOREIGN KEY (`regno`) REFERENCES `vehicle` (`regno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -66,6 +67,7 @@ CREATE TABLE `booking` (
 
 LOCK TABLES `booking` WRITE;
 /*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+INSERT INTO `booking` VALUES ('KA01 DP870','8','preet','14:51:09','28-11-2017'),('KA03 AC500','2','fool','14:03:06','28-11-2017'),('KA03 AC500','2','preet','2','2'),('KA03 AC500','3','fool','14:06:56','28-11-2017'),('KA03 AC500','4','fool','14:08:52','28-11-2017'),('KA03 AC500','5','fool','14:09:46','28-11-2017'),('KA04 SA404','7','fool','14:22:05','28-11-2017'),('KA08 BC123','8','preet','15:54:04','28-11-2017');
 /*!40000 ALTER TABLE `booking` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,18 +80,18 @@ DROP TABLE IF EXISTS `driverlicense`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `driverlicense` (
   `application_no` int(11) NOT NULL,
-  `rto` varchar(30) DEFAULT NULL,
+  `rto` varchar(40) DEFAULT NULL,
   `user` varchar(15) DEFAULT NULL,
   `fname` varchar(15) DEFAULT NULL,
   `lname` varchar(15) DEFAULT NULL,
   `mobile` char(10) DEFAULT NULL,
-  `perm_addr` varchar(50) DEFAULT NULL,
+  `perm_addr` varchar(100) DEFAULT NULL,
   `perm_city` varchar(20) DEFAULT NULL,
   `perm_pin` char(6) DEFAULT NULL,
   `gender` varchar(7) DEFAULT NULL,
   `email` varchar(20) DEFAULT NULL,
   `aadhar` varchar(15) DEFAULT NULL,
-  `temp_addr` varchar(50) DEFAULT NULL,
+  `temp_addr` varchar(100) DEFAULT NULL,
   `temp_city` varchar(15) DEFAULT NULL,
   `temp_pin` char(6) DEFAULT NULL,
   `dl_no` int(11) DEFAULT NULL,
@@ -110,7 +112,7 @@ CREATE TABLE `driverlicense` (
 
 LOCK TABLES `driverlicense` WRITE;
 /*!40000 ALTER TABLE `driverlicense` DISABLE KEYS */;
-INSERT INTO `driverlicense` VALUES (25,'KA04 - Yeshwanthpur, Bangalore','preet','Preetham','Maiya','9998887776','# 86','Blore','123455','null','null','null','null','null','null',525,'./uploads/preet~ID_Proof~51-1510504957108','./uploads/preet~Age_Proof~jpeg-1510504962355','true',57,'HMV'),(32,'KA05 - Jayanagar, Bangalore','preet','Preetham','Maiya','9999999998','No 98','Bangalore','789987','null','null','null','null','null','null',532,'./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134','false',59,'LMVNT'),(48,'KA04 - Yeshwanthpur, Bangalore','mallu','Malika','Makker','8256783234','#43, Sample Place','Sample City','560032','female','null','null','null','null','null',548,'./uploads/mallu~ID_Proof~52-1510556980544','./uploads/mallu~Age_Proof~52-1510556987402','false',63,'MCWG'),(49,'KA01 - Koramangala, Bangalore','mallu','Mallika','m','9999999999','#56, name','Blore','567765','null','null','null','null','null','null',549,'./uploads/mallu~ID_Proof~51-1510558683014','./uploads/mallu~Age_Proof~51-1510558686512','true',67,'MCWG');
+INSERT INTO `driverlicense` VALUES (25,'KA04 - Yeshwanthpur, Bangalore','preet','Preetham','Maiya','9998887776','# 86','Blore','123455','null','null','null','null','null','null',525,'./uploads/preet~ID_Proof~51-1510504957108','./uploads/preet~Age_Proof~jpeg-1510504962355','true',57,'HMV'),(32,'KA05 - Jayanagar, Bangalore','preet','Preetham','Maiya','9999999998','No 98','Bangalore','789987','null','null','null','null','null','null',532,'./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134','false',59,'LMVNT');
 /*!40000 ALTER TABLE `driverlicense` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,18 +125,18 @@ DROP TABLE IF EXISTS `learnerlicense`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `learnerlicense` (
   `application_no` int(11) NOT NULL,
-  `rto` varchar(30) DEFAULT NULL,
+  `rto` varchar(40) DEFAULT NULL,
   `user` varchar(30) DEFAULT NULL,
   `fname` varchar(15) DEFAULT NULL,
   `lname` varchar(15) DEFAULT NULL,
   `mobile` char(10) DEFAULT NULL,
-  `perm_addr` varchar(50) DEFAULT NULL,
+  `perm_addr` varchar(100) DEFAULT NULL,
   `perm_city` varchar(15) DEFAULT NULL,
   `perm_pin` char(6) DEFAULT NULL,
   `gender` varchar(7) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `aadhar` varchar(15) DEFAULT NULL,
-  `temp_addr` varchar(50) DEFAULT NULL,
+  `temp_addr` varchar(100) DEFAULT NULL,
   `temp_city` varchar(15) DEFAULT NULL,
   `temp_pin` char(6) DEFAULT NULL,
   `passed` varchar(5) DEFAULT 'false',
@@ -151,7 +153,7 @@ CREATE TABLE `learnerlicense` (
 
 LOCK TABLES `learnerlicense` WRITE;
 /*!40000 ALTER TABLE `learnerlicense` DISABLE KEYS */;
-INSERT INTO `learnerlicense` VALUES (54,'KA09 - Mysore','preet','Preetham','Maiya','9338447555','Hooli','Bangalore','345543','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',NULL),(56,'KA11 - Mandya','preet','Preet','Maiya','9999999999','hooli','jojo','456654','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',500),(57,'KA04 - Yeshwanthpur, Bangalore','preet','Preetham','Maiya','9998887776','# 86','Blore','123455','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',501),(58,'KA04 - Yeshwanthpur, Bangalore','preet','Preetham','Maiya','9998887766','no 89','Bangalore','560069','male','null','null','null','null','null','false','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',502),(59,'KA05 - Jayanagar, Bangalore','preet','Preetham','Maiya','9999999998','No 98','Bangalore','789987','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',503),(60,'KA05 - Jayanagar, Bangalore','layeeq','Layeeq','Ahmed','9998888744','#89','Bangalore','560778','null','null','null','null','null','null','true','./uploads/layeeq~ID_Proof~jpeg-1510556159067','./uploads/layeeq~Age_Proof~jpg-1510556155687',504),(61,'KA06 - Tumkur','layeeq','Layeeq','Ahmed','9998887776','#56','Bangalre','560099','null','null','null','null','null','null','false','./uploads/layeeq~ID_Proof~jpeg-1510556159067','./uploads/layeeq~Age_Proof~jpg-1510556155687',505),(62,'KA01 - Koramangala, Bangalore','layeeq','Layeeq','Ahmed','9988998877','#67','Bangalore','678987','null','null','null','null','null','null','false','./uploads/layeeq~ID_Proof~jpeg-1510556159067','./uploads/layeeq~Age_Proof~jpg-1510556155687',506),(63,'KA04 - Yeshwanthpur, Bangalore','mallu','Malika','Makker','8256783234','#43, Sample Place','Sample City','560032','female','null','null','null','null','null','true','./uploads/mallu~ID_Proof~51-1510558683014','./uploads/mallu~Age_Proof~51-1510558686512',507),(64,'KA04 - Yeshwanthpur, Bangalore','mallu','Malika','Makker','7129424592','#23, Sample House','Sample City','560032','female','null','null','null','null','null','false','./uploads/mallu~ID_Proof~51-1510558683014','./uploads/mallu~Age_Proof~51-1510558686512',508),(65,'KA04 - Yeshwanthpur, Bangalore','kru','Krutika','VH','7282828282','Peramant house number','perma city','560032','female','null','null','null','null','null','false','./uploads/kru~ID_Proof~jpeg-1510557239517','./uploads/kru~Age_Proof~52-1510557246607',509),(66,'KA04 - Yeshwanthpur, Bangalore','kru','Krutika','VH','8232400000','permanent house number','perma city','560032','others','null','null','null','null','null','false','./uploads/kru~ID_Proof~jpeg-1510557239517','./uploads/kru~Age_Proof~52-1510557246607',510),(67,'KA01 - Koramangala, Bangalore','mallu','Mallika','m','9999999999','#56, name','Blore','567765','null','null','null','null','null','null','true','./uploads/mallu~ID_Proof~51-1510558683014','./uploads/mallu~Age_Proof~51-1510558686512',511);
+INSERT INTO `learnerlicense` VALUES (54,'KA09 - Mysore','preet','Preetham','Maiya','9338447555','Hooli','Bangalore','345543','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',NULL),(56,'KA11 - Mandya','preet','Preet','Maiya','9999999999','hooli','jojo','456654','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',500),(57,'KA04 - Yeshwanthpur, Bangalore','preet','Preetham','Maiya','9998887776','# 86','Blore','123455','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',501),(58,'KA04 - Yeshwanthpur, Bangalore','preet','Preetham','Maiya','9998887766','no 89','Bangalore','560069','male','null','null','null','null','null','false','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',502),(59,'KA05 - Jayanagar, Bangalore','preet','Preetham','Maiya','9999999998','No 98','Bangalore','789987','null','null','null','null','null','null','true','./uploads/preet~ID_Proof~51-1510552611665','./uploads/preet~Age_Proof~51-1510552620134',503),(60,'KA05 - Jayanagar, Bangalore','layeeq','Layeeq','Ahmed','9998888744','#89','Bangalore','560778','null','null','null','null','null','null','true','./uploads/layeeq~ID_Proof~jpeg-1510556159067','./uploads/layeeq~Age_Proof~jpg-1510556155687',504),(61,'KA06 - Tumkur','layeeq','Layeeq','Ahmed','9998887776','#56','Bangalre','560099','null','null','null','null','null','null','false','./uploads/layeeq~ID_Proof~jpeg-1510556159067','./uploads/layeeq~Age_Proof~jpg-1510556155687',505),(62,'KA01 - Koramangala, Bangalore','layeeq','Layeeq','Ahmed','9988998877','#67','Bangalore','678987','null','null','null','null','null','null','false','./uploads/layeeq~ID_Proof~jpeg-1510556159067','./uploads/layeeq~Age_Proof~jpg-1510556155687',506),(65,'KA04 - Yeshwanthpur, Bangalore','kru','Krutika','VH','7282828282','Peramant house number','perma city','560032','female','null','null','null','null','null','false','./uploads/kru~ID_Proof~jpeg-1510557239517','./uploads/kru~Age_Proof~52-1510557246607',509),(66,'KA04 - Yeshwanthpur, Bangalore','kru','Krutika','VH','8232400000','permanent house number','perma city','560032','others','null','null','null','null','null','false','./uploads/kru~ID_Proof~jpeg-1510557239517','./uploads/kru~Age_Proof~52-1510557246607',510),(73,'KA01 - Koramangala, Bangalore','mallu','Lol','Lol','9999999998','#43','Blore','456654','null','null','null','null','null','null','false','./uploads/mallu~ID_Proof~jpg-1511863959597','./uploads/mallu~Age_Proof~jpg-1511863961946',517);
 /*!40000 ALTER TABLE `learnerlicense` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,6 +169,7 @@ CREATE TABLE `ll_type` (
   `user` varchar(15) NOT NULL,
   `type` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`no`,`user`),
+  UNIQUE KEY `user` (`user`,`type`),
   CONSTRAINT `ll_type_ibfk_1` FOREIGN KEY (`no`) REFERENCES `learnerlicense` (`application_no`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -177,7 +180,7 @@ CREATE TABLE `ll_type` (
 
 LOCK TABLES `ll_type` WRITE;
 /*!40000 ALTER TABLE `ll_type` DISABLE KEYS */;
-INSERT INTO `ll_type` VALUES (54,'preet','MC 50CC'),(56,'preet','HTV'),(57,'preet','HMV'),(58,'preet','HPMV'),(59,'preet','LMVNT'),(60,'layeeq','MCWG'),(61,'layeeq','FVG'),(62,'layeeq','HMV'),(63,'mallu','MCWG'),(64,'mallu','LMV'),(65,'kru','HTV'),(66,'kru','HPMV'),(67,'mallu','MCWG');
+INSERT INTO `ll_type` VALUES (66,'kru','HPMV'),(65,'kru','HTV'),(61,'layeeq','FVG'),(62,'layeeq','HMV'),(60,'layeeq','MCWG'),(73,'mallu','MC 50CC'),(57,'preet','HMV'),(58,'preet','HPMV'),(56,'preet','HTV'),(59,'preet','LMVNT'),(54,'preet','MC 50CC');
 /*!40000 ALTER TABLE `ll_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,7 +206,7 @@ CREATE TABLE `police_login` (
 
 LOCK TABLES `police_login` WRITE;
 /*!40000 ALTER TABLE `police_login` DISABLE KEYS */;
-INSERT INTO `police_login` VALUES ('preet','maiya','Preetham','Maiya');
+INSERT INTO `police_login` VALUES ('fool','foo','Foo','Bar'),('preet','maiya','Preetham','Maiya');
 /*!40000 ALTER TABLE `police_login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -229,7 +232,7 @@ CREATE TABLE `user_login` (
 
 LOCK TABLES `user_login` WRITE;
 /*!40000 ALTER TABLE `user_login` DISABLE KEYS */;
-INSERT INTO `user_login` VALUES ('foo','foo','Foo','Bar'),('kirti','kirtisingh','Kirti','Singh'),('kru','krutika','Krutika','VH'),('layeeq','layeeq','Layeeq','Ahmed'),('mallu','iammallu','Malika','Makker'),('preet','maiya','Preetham','Maiya');
+INSERT INTO `user_login` VALUES ('foo','foo','Foo','Bar'),('kirti','kirtisingh','Kirti','Singh'),('kru','krutika','Krutika','VH'),('layeeq','layeeq','Layeeq','Ahmed'),('mallu','iammallu','Malika','Makker'),('preet','maiya','Preetham','Maiya'),('preet123','maiya','Preetham','Maiya');
 /*!40000 ALTER TABLE `user_login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -261,8 +264,10 @@ CREATE TABLE `vehicle` (
   `engine_no` varchar(30) DEFAULT NULL,
   `ownership` varchar(25) DEFAULT NULL,
   `vehicle_type` varchar(13) DEFAULT NULL,
-  PRIMARY KEY (`application_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `regno` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`application_no`),
+  UNIQUE KEY `regno` (`regno`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -271,7 +276,7 @@ CREATE TABLE `vehicle` (
 
 LOCK TABLES `vehicle` WRITE;
 /*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
-INSERT INTO `vehicle` VALUES (1,'KA03 - Indiranagar, Bangalore','preet','false','Preetha','AMaiy','9998887776','45','Blore','123456','null','null','null','null','null','null','3452','21432','FIRM','undefined'),(2,'KA04 - Yeshwanthpur, Bangalore','mallu','false','Malika','Makker','9292929292','sample add','sampl','560023','female','null','null','null','null','null','23423525','23423235','INDIVIDUAL','NON-TRANSPORT'),(3,'KA04 - Yeshwanthpur, Bangalore','layeeq','false','Layeeq','Ahmed','9879879876','my house','mycity','560032','male','null','null','null','null','null','84483929','2929492','INDIVIDUAL','NON-TRANSPORT'),(4,'KA04 - Yeshwanthpur, Bangalore','layeeq','false','Layeeq','Ahmed','9879879876','perma house','mycity','560021','male','null','null','null','null','null','999213312','999234324','INDIVIDUAL','TRANSPORT'),(5,'KA01 - Koramangala, Bangalore','mallu','false','Preetham','Maiya','9999999999','#45','Blore','789987','null','null','null','null','null','null','5676','5678','INDIVIDUAL','NON-TRANSPORT');
+INSERT INTO `vehicle` VALUES (1,'KA03 - Indiranagar, Bangalore','preet','true','Preetha','AMaiy','9998887776','45','Blore','123456','null','null','null','null','null','null','3452','21432','FIRM','undefined','KA03 AC500'),(2,'KA04 - Yeshwanthpur, Bangalore','mallu','true','Malika','Makker','9292929292','sample add','sampl','560023','female','null','null','null','null','null','23423525','23423235','INDIVIDUAL','NON-TRANSPORT','KA04 SA404'),(3,'KA04 - Yeshwanthpur, Bangalore','layeeq','false','Layeeq','Ahmed','9879879876','my house','mycity','560032','male','null','null','null','null','null','84483929','2929492','INDIVIDUAL','NON-TRANSPORT',NULL),(4,'KA04 - Yeshwanthpur, Bangalore','layeeq','false','Layeeq','Ahmed','9879879876','perma house','mycity','560021','male','null','null','null','null','null','999213312','999234324','INDIVIDUAL','TRANSPORT',NULL),(5,'KA01 - Koramangala, Bangalore','mallu','true','Preetham','Maiya','9999999999','#45','Blore','789987','null','null','null','null','null','null','5676','5678','INDIVIDUAL','NON-TRANSPORT','KA01 DP870'),(6,'KA08 - Kolar Gold Fields','preet123','true','Preetham','Maiya','8765432101','#45','Bangalore','678987','male','null','null','null','null','null','12345','23445','INDIVIDUAL','NON-TRANSPORT','KA08 BC123');
 /*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -310,4 +315,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-26 22:46:15
+-- Dump completed on 2017-11-28 18:00:04
